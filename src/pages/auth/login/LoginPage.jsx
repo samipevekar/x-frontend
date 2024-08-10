5a90e8b901db2d54dae94a7419de33fae5dc7ead
samipevekar <samipevekar499@gmail.com>
2024-08-10 10:30:19 +0530

    login and singup page changes

--- src/pages/auth/login/LoginPage.jsx
+++ src/pages/auth/login/LoginPage.jsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
 				const data = await res.json()
 				if(!res.ok) throw new Error(data.error || "Failed to login")
 				console.log(data)
-				
+				return data
 			} catch (error) {
 				console.error(error)
 				throw new Error(error)
